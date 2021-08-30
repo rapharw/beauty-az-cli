06595db377f8241af4698f87c60c25cc054048b1
Raphael Wanderley <rapha.rw@gmail.com>
2021-08-30 16:01:54 -0300

    list resource groups

---
+++ src/app/wizards/az-cli/operations/az-group/list/operation-index.ts
@@ -0,0 +1,25 @@
+import Choice from "../../../../../../lib/inquirer/choice";
+import OperationIndex from "../../../../../operation-index";
+import subscriptionQuestionSelect from "../../shared/subscription-question-select";
+import Group from "../group";
+
+export default class AzCliAzGroupListOperationIndex  extends OperationIndex{
+
+    getChoice(): Choice {
+        return new Choice(
+            `${process.env.AZCLI_AZ_GROUP_LIST_CHOICE_NAME}`,
+            `${process.env.AZCLI_AZ_GROUP_LIST_CHOICE_SHORT}`,
+            `${process.env.AZCLI_AZ_GROUP_LIST_CHOICE_VALUE}`);
+    }
+
+    async execute (): Promise<any> {
+
+        // subscription
+        const subscriptionsSelect = await subscriptionQuestionSelect();
+        const subscriptionResult = await subscriptionsSelect.ask();
+
+        const group = new Group();
+        return await group.listResourceGroups(subscriptionResult.subscription).then(console.log);
+    }
+
+}
